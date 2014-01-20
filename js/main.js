a54b91b97705f35a5cc73670a808a53613827a6e
Plou <cl.plou@gmail.com>
2014-01-20 23:07:22 +0100

    [upd] Added grunt configuration to enable stand alone use [fix] Removed compiled javascript files from gitignore to keep compatibility with generator-dummies

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(function() {
+  $(function() {
+    return $(window).ready(function() {
+      $(".no-touch img.hover").hoverSrc();
+      if (!Modernizr.input.placeholder) {
+        return $("input").each(function() {
+          if ($(this).attr("placeholder") !== "") {
+            return $(this).placeholder();
+          }
+        });
+      }
+      /*
+      
+      # Add backToTop anchor when half a screen  is scrolled
+      $('body').append('<a id="backToTop" href="#">Back to top</a>')
+      $('#backToTop').backToTop($(window).height()/2)
+      
+      # Handle pulldown
+      $('.pulldown').pulldown()
+      
+      # Refresh scroll offset of backToTop button appearance
+      $(window).bind 'resize', ->
+        $('#backToTop').backToTop($(window).height()/2)
+      */
+
+    });
+  });
+
+}).call(this);
+
+/*
+//@ sourceMappingURL=main.js.map
+*/
